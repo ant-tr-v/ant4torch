e2f5116cb9a39b1c6a9c1cb10232e809531f5e2c
Anton <trusov.anton.02@gmail.com>
2025-02-08 16:35:44 +0300

    datasets loading and visualization + readme

---
+++ a4t_datasets.py
@@ -0,0 +1,123 @@
+import os
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+
+def load(dataset='mnist', dataset_root='./datasets', batch_size=32, image_size=None):
+    # Define default sizes for datasets
+    default_sizes = {
+        'mnist': 28,
+        'emnist': 28,
+        'cifar': 32,
+        'imagenette': 128
+    }
+    dataset = dataset.lower()
+    if image_size is None:
+        image_size = default_sizes.get(dataset, 128)  # Default to 128 if dataset unknown
+
+    
+    transform_test = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+    ])
+    
+    if dataset == 'mnist':
+        transform_train = transforms.Compose([
+            transforms.RandomAffine(degrees=10, translate=(2/ image_size, 2 / image_size)),
+            transforms.ToTensor(),
+        ])
+        train_dataset = torchvision.datasets.MNIST(root=dataset_root, train=True, download=True, transform=transform_train)
+        test_dataset = torchvision.datasets.MNIST(root=dataset_root, train=False, download=True, transform=transform_test)
+    elif dataset == 'emnist':
+        transform_train = transforms.Compose([
+            transforms.RandomAffine(degrees=10, translate=(2/ image_size, 2 / image_size)),
+            transforms.ToTensor(),
+            lambda x: x.permute(0, 2, 1)
+        ])
+        transform_test = transforms.Compose([
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+            lambda x: x.permute(0, 2, 1)
+        ])
+        train_dataset = torchvision.datasets.EMNIST(root=dataset_root, split='letters', train=True, download=True, transform=transform_train)
+        test_dataset = torchvision.datasets.EMNIST(root=dataset_root, split='letters', train=False, download=True, transform=transform_test)
+    elif dataset == 'cifar':
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(image_size, padding = 4 * image_size / 32),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ])
+        train_dataset = torchvision.datasets.CIFAR10(root=dataset_root, train=True, download=True, transform=transform_train)
+        test_dataset = torchvision.datasets.CIFAR10(root=dataset_root, train=False, download=True, transform=transform_test)
+    elif dataset == 'imagenette':
+        transform_train = transforms.Compose([
+            transforms.RandomResizedCrop(image_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ])
+        if not os.path.exists(f'{dataset_root}/imagenette2'):
+            train_dataset = torchvision.datasets.Imagenette(dataset_root, split="train", download=True, target_transform=transform_train)
+        else:
+            train_dataset = torchvision.datasets.ImageFolder(root=f'{dataset_root}/imagenette2/train', transform=transform_train)
+        test_dataset = torchvision.datasets.ImageFolder(root=f'{dataset_root}/imagenette2/val', transform=transform_test)
+    else:
+        raise ValueError(f"Unsupported dataset: {dataset}")
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    return train_loader, test_loader
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_sample_grid(data_loader, grid_size=(5, 5), fig_size=None, verbose=0):
+    CLASS_LENGTH_LIMIT = 15
+
+    dataset = data_loader.dataset
+    num_samples = grid_size[0] * grid_size[1]
+
+    if fig_size is None:
+        fig_size = (grid_size[1] * 1.5, grid_size[0] * 1.5)  # Default fig size based on grid size
+
+    fig, axes = plt.subplots(grid_size[0], grid_size[1], figsize=fig_size)
+    axes = axes.flatten()
+
+    used_classes = set()
+    class_names = getattr(dataset, 'classes', None)
+
+    max_class_length = max((len(name) for name in class_names), default=0) if class_names else -1
+
+    for i in range(num_samples):
+        # getting image as nparray
+        img, label = dataset[np.random.randint(len(dataset))]
+        img = img.numpy() if not isinstance(img, np.ndarray) else img
+        # plotting image
+        if img.shape[0] == 3:  # RGB Image
+            img = np.transpose(img, (1, 2, 0))
+            axes[i].imshow(img)
+        elif img.shape[0] == 1:  # Grayscale Image
+            axes[i].imshow(img[0], cmap='gray')
+        else:
+            raise RuntimeError(f'Incorrect number of channels: {img.shape[0]}')
+        # adding label
+        used_classes.add(label)
+        if max_class_length > CLASS_LENGTH_LIMIT or max_class_length < 0:
+            axes[i].set_title(f"Class #{label}") # can't print class name on figure
+        else:
+            axes[i].set_title(f"{class_names[label]}")
+
+        axes[i].axis("off")
+
+    fig.tight_layout()
+
+    if verbose and max_class_length > CLASS_LENGTH_LIMIT:
+        print("Class legend:")
+        for label in sorted(used_classes):
+            class_name = class_names[label] if class_names else f"Class #{label}"
+            print(f"{label}: {class_name}")
+
+    return fig, axes
---
+++ readme.md
@@ -0,0 +1,16 @@
+# ant4torch
+
+
+## A bit of history
+
+I often experiment with different models in PyTorch to understand how various features affect model quality.
+
+At some point, I realized that many code fragments kept moving from one experiment to another.
+Sometimes I update or improve them.
+Sometimes I just use them as they are.
+
+But many tasks (such as loading and augmenting datasets, logging, and visualizing results) are just routine.
+I don't want to rewrite or redesign them every time.
+I only want to do this once!
+
+That’s how I decided to create this repository. 😊
